ac339b34ceded4443673ea06bc9c2405e150fe36
0xPr0f <0xprofoz@gmail.com>
2022-04-14 17:47:01 +0100

    comits

---
+++ programs/solanaXrust/src/lib.rs
@@ -0,0 +1,47 @@
+/*use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod solana_xrust {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
+*/
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod solana_xrust {
+    use super::*;
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+}
+
+// Attach certain variables to the StartStuffOff context.
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
---
+++ tests/solanaXrust.js
@@ -0,0 +1,65 @@
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+/*
+describe("solanaXrust", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const program = anchor.workspace.SolanaXrust;
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
+*/
+const main = async () => {
+  console.log("🚀 Starting test...");
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.SolanaXrust;
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+  /*
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+  */
+
+  let tx = await program.methods.initialize().rpc({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
+  // const tx = await program.methods.initialize().rpc();
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  console.log(baseAccount.publicKey);
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.total_gifs.toString());
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
